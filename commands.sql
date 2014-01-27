fe03e546ceb8f27761107a25b15ed072575d762a
Alex Sims <alexmk92@live.co.uk>
2014-01-27 15:27:06 +0000

    Added initial SQL query to demonstrate functionality in next fortnightly meeting

---
+++ commands.sql
@@ -0,0 +1,8 @@
+/* Show a list of all rooms which pertain to a property */
+SELECT addresses.addr_line_1, addresses.addr_postcode, 
+       cities.city_name, room_price, room_details
+FROM rooms
+JOIN properties ON properties.property_id = rooms.property_id
+JOIN addresses ON addresses.addr_id = properties.prop_addr
+JOIN cities ON addresses.addr_id = cities.city_id
+WHERE rooms.property_id = 1;
